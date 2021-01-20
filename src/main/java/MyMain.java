003f7d7b56e40d060908d3fa109d75f6539acc7f
MetalTurtle <dekolis1218@gmail.com>
2021-01-20 14:40:23 -0800

    Create battleship code and inOrder method

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,8 +3,13 @@ public class MyMain {
     // was a boat the specified coordinates. This
     // method also prints out an appropriate message
     public static boolean hit(boolean[][] board, int row, int col) { 
-        // YOUR CODE HERE
-        return false;
+        if (board[row][col]) {
+            System.out.println("There was a hit!");
+            return true;
+        } else {
+            System.out.println("You missed a boat!");
+            return false;
+        }
     }
 
 
@@ -13,7 +18,15 @@ public class MyMain {
     // The remaining pieces are placed in the direction given
     // by the direction input
     public static boolean[][] placeBoat(boolean[][] board, String direction, int boatLength, int row, int col) { 
-        // YOUR CODE HERE
+        if (direction.equals("right")) {
+            for (int i = 0; i < boatLength; i++) {
+                board[row][col + i] = true;
+            }
+        } else {
+            for (int i = 0; i < boatLength; i++) {
+                board[row + i][col] = true;
+            }
+        }
         return board;
     }
 
@@ -22,8 +35,16 @@ public class MyMain {
     // increasing length
     // You may assume that all Strings are lowercase 
     public static boolean inOrder(String[][] words) { 
-        // YOUR CODE HERE
-        return false;
+        for (String[] row : words) {
+            for (int i = 0; i < row.length - 1; i++) {
+                if (row[i].length() > row[i + 1].length()) {
+                    return false;
+                } else if (row[i].length() == row[i + 1].length() && row[i].compareTo(row[i + 1]) < 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
